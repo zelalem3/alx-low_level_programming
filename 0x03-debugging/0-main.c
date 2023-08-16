85a310e81087a495104c22f1bb645b030ace6988
zelalem3 <zgetnet24@example.com>
2023-08-16 11:18:50 -0700

    debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,13 @@
 #include "main.h"
-
 /**
- * Main - Test function for positive or negative.
- *
- * Return: 0
- */
-
+* main - Test function for positive or negative
+* Return: 0
+*/
 int main(void)
 {
-	int i = 0;
-
-	positive_or_negative(i);
-	return (0);
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
 }
+
