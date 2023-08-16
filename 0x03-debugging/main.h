08616614cc47ba14fe22c08ba3e006ea657f04a5
zelalem3 <zgetnet24@example.com>
2023-08-16 10:45:11 -0700

    debug

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
+int largest_number(int i, int j, int k);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
