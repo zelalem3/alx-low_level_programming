ffe75f668c1d5fe2f65ad0f2691bf87f7aeae722
zelalem3 <zgetnet24@example.com>
2023-08-16 10:53:27 -0700

    debug

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
+
 /**
-* main - causes an infinite loop
-* Return: 0
-*/
+ * Main - causes an infinite loop
+ * Return: 0
+ */
+
 int main(void)
 {
-int j;
-printf("Infinite loop incoming :(\n");
-j = 0;
-/*while (j < 10)*/
-/*{*/
-/*putchar(j);*/
-/*}*/
-printf("Infinite loop avoided! \\o/\n");
-return (0);
+	int i;
+
+	printf("ifinte loop incoming :(\n");
+
+	i = 0;
+
+	/*While (i < 10)*/
+	/*{*/
+	/*   putchar(i);*/
+	/*}*/
+
+	printf("Infinte loop avoided! \\o/\n");
+
+	return (0);
 }
