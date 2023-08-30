7522e78ffaf754eac3a63b6a21c545e6e5e2777f
root <zelalem3>
2023-08-30 07:20:04 -0700

    Z

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,8 +1,9 @@
+
 #ifndef MAIN_H
 #define MAIN_H
-int wildcmp(char *s1, char *s2);
-int _putchar(char c);
+
 void _puts_recursion(char *s);
+int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
@@ -10,6 +11,4 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
-int findsrc(char *s, char c, int i, int p);
-
-#endif /* MAIN_H *
+#endif
