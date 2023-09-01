54118e55dd947357a2ae361af100d017fedb4868
root <zelalem3>
2023-09-01 04:45:08 -0700

    z

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -24,8 +24,6 @@ char *_strcat(char *dest, char *src)
 		i++;
 		j++;
 	}		
-
-
 	dest[i] = '\0';
 	return (dest);
 }
--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -8,13 +8,10 @@
  */
 int _atoi(char *s)
 {
- 	int sign = 1, i = 0;
- 	unsigned int res = 0;
-
-
- 	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
- 	{
- 		if (s[i] == '-')
+int sign = 1, i = 0;
+unsigned int res = 0;
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')	
+	{ 		if (s[i] == '-')
  			sign *= -1;
  		i++;
  	}
