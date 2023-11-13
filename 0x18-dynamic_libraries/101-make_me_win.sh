b1c7e4fbfa1ade2ec14f97340f778682d2452df9
zelalem3 <zgetnet24@example.com>
2023-11-13 07:32:03 -0800

    l

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -F /tmp https://github.com/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/alx-low_level_programming/raw/master/0*18-dynameic_libraries/libgiga.so
+wget -F /tmp https://github.com/zelalem3/alx-low_level_programming/raw/master/0*18-dynameic_libraries/libgiga.so
+export LD_PRELOAD=/tmp/libgiga.c
